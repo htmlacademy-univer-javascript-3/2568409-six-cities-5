d432cf5c18a0490c70c4c494f9d5b15df8af5a28
Vitaliy Dmitriev <dmitriz_vitaliy@mail.ru>
2025-06-07 13:40:11 +0500

    Больше подробностей (часть 1)

---
+++ src/mocks/cities.ts
@@ -0,0 +1,11 @@
+import {ICity} from '../types.ts';
+
+export const cities: ICity[] = [
+  {
+    name: 'Amsterdam',
+    location: {
+      latitude: 52.374,
+      longitude: 4.889969
+    }
+  },
+];
